13f40dec428f0229b4fd70df3ba1617ce494738a
Oleksii Talakh <oleksii.talakh@gmail.com>
2022-05-14 15:43:41 +0300

    Update HashTable java doc

--- 3-data-structure/src/main/java/org/course/HashTable.java
+++ 3-data-structure/src/main/java/org/course/HashTable.java
@@ -1,5 +1,19 @@
 package org.course;
 
+/**
+ * A simple implementation of the Hash Table that allows storing a generic key-value pair. The table itself is based
+ * on the array of {@link Node} objects.
+ * <p>
+ * An initial array capacity is 16.
+ * <p>
+ * Every time a number of elements is equal to the array size that tables gets resized
+ * (it gets replaced with a new array that it twice bigger than before). E.g. resize operation will replace array
+ * of size 16 with a new array of size 32. PLEASE NOTE that all elements should be reinserted to the new table to make
+ * sure that they are still accessible  from the outside by the same key.
+ *
+ * @param <K> key type parameter
+ * @param <V> value type parameter
+ */
 public class HashTable<K, V> {
 
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
@@ -19,10 +33,13 @@ public class HashTable<K, V> {
     }
 
     /**
-     * Adds an element to the hash table. Does not support duplicate elements.
+     * Puts a new element to the table by its key. If there is an existing element by such key then it gets replaced
+     * with a new one, and the old value is returned from the method. If there is no such key then it gets added and
+     * null value is returned.
      *
-     * @param element
-     * @return true if it was added
+     * @param key   element key
+     * @param value element value
+     * @return old value or null
      */
     public V put(K key, V value) {
         if (size >= arr.length * LOAD_FACTOR){
@@ -63,10 +80,10 @@ public class HashTable<K, V> {
     }
 
     /**
-     * Prints a hash table according to the following format
-     * 0: Andrii -> Taras
-     * 1: Start
-     * 2: Serhii
+     * Prints a content of the underlying table (array) according to the following format:
+     * 0: key1:value1 -> key2:value2
+     * 1:
+     * 2: key3:value3
      * ...
      */
     public void printTable() {
@@ -84,9 +101,6 @@ public class HashTable<K, V> {
         }
     }
 
-    /**
-     * Creates a new underlying table with a given size and add all elements to the new table.
-     */
     private void resize() {
         Node<K, V>[] oldArr = arr;
         int oldSize = size;
